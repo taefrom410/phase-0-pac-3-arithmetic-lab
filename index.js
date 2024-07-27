369d3b698758c726de82f846bd15f90fcae59bda
duartae <taebaby410@gmail.com>
2024-07-27 17:17:45 -0400

    fixed all the problems

---
+++ index.js
@@ -0,0 +1,43 @@
+1 + 80
+60 - 40
+2 * 3.4
+5.0 / 2.5
+function multiply(){}
+function add(a, b) {
+    return a + b
+  }
+function divide (a,b){
+return a / b
+}
+function subtract (a,b){
+return a - b }
+function multiply (a,b){
+return a * b
+}
+number = 10
+function add5(){
+return (number += 5)
+}
+function divideBy3() {
+    return (number /= 3);
+  }
+divideBy3()
+add5()
+divideBy3()
+parseInt("2", 10)
+parseInt("2.2222", 10)
+parseInt("nonsense!", 10)
+function increment(n) {
+  return n + 1;
+}
+
+function makeInt(n) {
+  return parseInt(n, 10);
+}
+
+function decrement(n) {
+  return n - 1;
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n) }
